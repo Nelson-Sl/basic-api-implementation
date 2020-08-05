5ff52fab0c021a3363fefbb045e12fc358478348
Shen Liao <shen.liao@thoughtworks.com>
2020-08-05 14:34:49 +0800

    [Shen Liao] feat: System can now detect ages over 100

--- src/main/java/com/thoughtworks/rslist/domain/User.java
+++ src/main/java/com/thoughtworks/rslist/domain/User.java
@@ -18,6 +18,7 @@ Basic Input Example
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -29,6 +30,7 @@ public class User {
     @NotNull
     private String userName;
     @Min(18)
+    @Max(100)
     private int age;
     @NotNull
     private String gender;
