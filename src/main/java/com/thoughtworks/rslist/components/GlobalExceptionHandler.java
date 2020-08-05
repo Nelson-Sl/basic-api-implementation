84c146c490ece7ac7fb3e688c67499ddf3024b8d
Shen Liao <shen.liao@thoughtworks.com>
2020-08-06 01:58:58 +0800

    [Shen Liao] feat: Can apply logger to print the error log on get request

--- src/main/java/com/thoughtworks/rslist/components/GlobalExceptionHandler.java
+++ src/main/java/com/thoughtworks/rslist/components/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package com.thoughtworks.rslist.components;
 import com.thoughtworks.rslist.exception.CommonError;
 import com.thoughtworks.rslist.exception.InvalidIndexInputException;
 import com.thoughtworks.rslist.exception.InvalidRequestParamException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler({InvalidIndexInputException.class, MethodArgumentNotValidException.class,
             InvalidRequestParamException.class})
     public ResponseEntity exceptionHandler(Exception iie) {
@@ -23,6 +27,7 @@ public class GlobalExceptionHandler {
         }
 
         commonError.setError(errorMessage);
+        logger.error(errorMessage);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(commonError);
     }
 }
